08d635fa27a80af3fda83a9d3fc7e8db2188daea
Ethan Coe-Renner <e.coerenner@gmail.com>
2022-12-12 19:56:04 -0800

    this runs

--- preprocess.py
+++ preprocess.py
@@ -10,24 +10,34 @@ transformation = transforms.Compose([
                          (0.2470, 0.2435, 0.2616)),
 
     # TODO: figure out a reasonable value here
-    transforms.CenterCrop(500)
+    transforms.CenterCrop(32)
 ])
 
-def transform(sample):
-    image, label = sample["images"], sample["labels"]
-    image = transformation(image)
-    return image, label
+wikiart = ds.pytorch(
+    transform={'images': transformation, 'labels': None},
+    tensors=["images", "labels"],
+    # decode_method={'images':'pil', 'labels': 'numpy'},
+    shuffle=True,
+    batch_size=32)
 
-wikiart = ds.pytorch(transform=transform, tensors=["images", "labels"], decode_method={'images':'pil', 'labels': 'numpy'}, shuffle=True, batch_size=32)
+# We expect that this should return a loader, next returns a list with two elements
+# first is a tensor of 32 images
+# second is a tensor of 32 integers representing the labels of that batch
 
+# for images, labels in wikiart:
+#     print("Images: ", images)
+#     print("labels: ", type(labels))
+#     break
+
+
+# given this, this should not work vvv. We need to do this before the
 # TODO: map these onto the actual values we are checking, this needs to be changed because labels is a tensor, not a number
-label_map = {4:0, 5:1, 7:2}
-class_names = ["romantic", "name2", "name3"]
+#label_map = {4:0, 5:1, 7:2}
+#class_names = ["romantic", "name2", "name3"]
 # dataset = [(img, label_map[label])
 #       for img, label in wikiart if label in [4,5,7] # TODO change this last list too
 #       ]
 
-'''
 # Make the network
 import torch.nn as nn
 import torch.nn.functional as F
@@ -41,7 +51,7 @@ class Net(nn.Module):
 
         self.ch = 4 * 4 * (n//2)
         self.fc1 = nn.Linear(self.ch, 32)
-        self.fc2 = nn.Linear(32, 3)
+        self.fc2 = nn.Linear(32, 27)
 
     def forward(self, x):
         out = F.max_pool2d(torch.tanh(self.conv1(x)), 2)
@@ -54,15 +64,45 @@ class Net(nn.Module):
         return out
 
 
+def training_function(train_loader, model, loss_fn, optimizer, device, n_epochs):
+    for epoch in range(1, n_epochs + 1):
+        loss_train = 0.0
+        for imgs, labels in train_loader:
+            labels = labels.flatten() # this is needed because for some reason every label is in a seperate tensor
+            print(labels)
+            print(imgs.shape)
+            imgs = imgs.to(device=device)
+            labels = labels.to(device=device)
+            outputs = model(imgs)
+            loss = loss_fn(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            loss_train += loss.item()
+
+        if epoch == 1 or epoch % 10 == 0:
+            print('{} Epoch {}, Training loss {}'.format(
+                datetime.datetime.now(), epoch,
+                loss_train / len(train_loader)))
+
+
 cnn32 = Net(32)
 
 import torch
-loader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
 
-size = 0
-for _, _ in dataset:
-    size += 1
+import torch.optim as optim
 
-print("batches in dataset:", size)
 
-'''
+device = (torch.device('cuda') if torch.cuda.is_available()
+                      else torch.device('cpu'))
+
+training_function(
+    train_loader = wikiart,
+    model = cnn32,
+    loss_fn = nn.NLLLoss(),
+    optimizer = optim.SGD(cnn32.parameters(), lr=1e-2),
+    device=device,
+    n_epochs = 2,
+)
